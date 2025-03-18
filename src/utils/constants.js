3245689f78a1a51106b7daeb60e49823faf0de77
Ishank Suri <ishanksuri01@gmail.com>
2025-03-18 20:47:47 +0530

    UPdated BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+
+// "http://localhost:3000" is configured in nginx config file
+// This is relative path, takes server ip automatically before /api
+//eg: now login will work like this http://3.109.203.151/api/login
+//eg2 http://3.109.203.151/api/profile/view
+export const BASE_URL = "/api";
